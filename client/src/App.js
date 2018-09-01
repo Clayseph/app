ee7fcf83b585000783af98ff8c715f1ec55a7b1d
Clayton <clayton.yarborough@gmail.com>
2018-09-01 14:37:17 -0400

    lint

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,93 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import {
+    AppBar, Drawer, IconButton, Divider, ListItem, ListItemText
+} from '@material-ui/core';
+import { ChevronLeft, Menu } from '@material-ui/icons';
 import './App.css';
 import WorkoutConfig from './Workout/WorkoutConfig';
 import Tracker from './Workout/Tracker';
-import { AppBar, Drawer, IconButton, Divider, ListItem, ListItemText} from '@material-ui/core';
-import {ChevronLeft, Menu} from '@material-ui/icons';
 
 class App extends Component {
-  state = {
-    users:[],
-    page: 'tracker',
-    open: true
-  }
-  constructor(props){
-    super(props);
-    this.switchToTrackerPage = this.switchToTrackerPage.bind(this);
-    this.switchToConfigPage = this.switchToConfigPage.bind(this);
-    this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
-    this.handleDrawerClose = this.handleDrawerClose.bind(this);
-  }
-  componentDidMount(){
+    constructor(props) {
+        super(props);
+        state = {
+            users: [],
+            page: 'tracker',
+            open: false
+        };
+        this.switchToTrackerPage = this.switchToTrackerPage.bind(this);
+        this.switchToConfigPage = this.switchToConfigPage.bind(this);
+        this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
+        this.handleDrawerClose = this.handleDrawerClose.bind(this);
+    }
 
-    fetch('/users')
-    .then(res => res.json())
-    .then(users => this.setState({users}));
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }));
     // fetch('/users/add',{
     //   method:'POST'
     // })
-  }
-  switchToTrackerPage(){
-    this.setState({
-      page:'tracker'
-    })
-  }
-  switchToConfigPage(){
-    this.setState({
-      page:'workoutConfig'
-    })
-  }
-  handleDrawerOpen(){
-    this.setState({open: true});
-  }
-  handleDrawerClose(){
-    this.setState({open: false});
-  }
-  render() {
+    }
 
-    return (
-      <div className="App">
-      <AppBar>
-      <IconButton
-                color="inherit"
-                aria-label="Open drawer"
-                onClick={this.handleDrawerOpen}
-              >
-                <Menu />
-              </IconButton>
-        <Drawer
-        variant="persistent"
-        anchor={'left'}
-        open={this.state.open}
-        >
-        <div>
-          <IconButton onClick={this.handleDrawerClose}>
-            <ChevronLeft/>
-          </IconButton>
-        </div>
-        <Divider/>
-        <ListItem button onClick={this.switchToConfigPage}>
-          <ListItemText primary="Add Workout" />
-        </ListItem>
-        <ListItem button onClick={this.switchToTrackerPage}>
-          <ListItemText primary="My Workouts" />
-        </ListItem>
-        </Drawer>
+    switchToTrackerPage() {
+        this.setState({
+            page: 'tracker'
+        });
+    }
+
+    switchToConfigPage() {
+        this.setState({
+            page: 'workoutConfig'
+        });
+    }
+
+    handleDrawerOpen() {
+        this.setState({ open: true });
+    }
+
+    handleDrawerClose() {
+        this.setState({ open: false });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <AppBar>
+                    <IconButton
+                      color="inherit"
+                      aria-label="Open drawer"
+                      onClick={this.handleDrawerOpen}
+                    >
+                        <Menu />
+                    </IconButton>
+                    <Drawer
+                      variant="persistent"
+                      anchor="left"
+                      open={this.state.open}
+                    >
+                        <div>
+                            <IconButton onClick={this.handleDrawerClose}>
+                                <ChevronLeft />
+                            </IconButton>
+                        </div>
+                        <Divider />
+                        <ListItem button onClick={this.switchToConfigPage}>
+                            <ListItemText primary="Add Workout" />
+                        </ListItem>
+                        <ListItem button onClick={this.switchToTrackerPage}>
+                            <ListItemText primary="My Workouts" />
+                        </ListItem>
+                    </Drawer>
         Workout Tracker
-        </AppBar>
-        <div className="content">
-          {this.state.page ==='workoutConfig' ? <WorkoutConfig users={this.state.users}/> : null }
-          {this.state.page ==='tracker' ? <Tracker/> : null }
-        </div>
-      </div>
-    );
-  }
+                </AppBar>
+                <div className="content">
+                    {this.state.page === 'workoutConfig' ? <WorkoutConfig users={this.state.users} /> : null }
+                    {this.state.page === 'tracker' ? <Tracker /> : null }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
